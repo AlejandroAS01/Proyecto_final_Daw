532c2d47c7c01d1057b8d0bac1ead810bb24d113
AlejandroAS01 <Alejandroaguilarsabuco@hotmail.com>
2024-06-05 15:16:20 +0000

    Ordenar anuncios

--- Lost_and_found_Pet/views.py
+++ Lost_and_found_Pet/views.py
@@ -3,7 +3,7 @@ from Anuncios.models import AnuncioMascota
 
 # Devuelve la petición del home.html que lo obtiene del template
 def home(request):
-    anuncios = AnuncioMascota.objects.all()
+    anuncios = AnuncioMascota.objects.order_by('-id')[:3]
     return render(request, 'home.html', {'anuncios': anuncios})
 
 def aviso_legal(request):
@@ -13,11 +13,11 @@ def contacto(request):
     return render(request, 'contacto.html')
 
 def mi_usuario(request):
-    anuncios = AnuncioMascota.objects.all()
+    anuncios = AnuncioMascota.objects.all().order_by('id')
     return render(request, 'mi_usuario.html',{'anuncios': anuncios})
 
 def anuncios(request):
-    anuncios = AnuncioMascota.objects.all()
+    anuncios = AnuncioMascota.objects.all().order_by('id')
     return render(request, 'anuncios.html',{'anuncios': anuncios})
 
 
